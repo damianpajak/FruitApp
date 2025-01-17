46b53fd7ba626608c8f0761135715ff3504c83c7
Damian Pająk <damian.pajak01@gmail.com>
2025-01-17 14:08:53 +0100

    Add new component and rename already exist

--- src/pages/Fruits.jsx
+++ src/pages/Fruits.jsx
@@ -1,9 +1,19 @@
-import { FruitInfo } from "../components/FruitsPage/FruitInfo";
+import { FruitSearch } from "../components/FruitsPage/FruitSearch";
+import { FruitTable } from "../components/FruitsPage/FruitTable";
+import { useFilterFruitData } from "../customHooks/UseFilterFruitData";
 
 export const Fruits = () => {
+  const { searchFruit, filterFruits, handleSearchChange } =
+    useFilterFruitData();
+
   return (
-    <div className="container">
-      <FruitInfo />
+    <div className="fruits container">
+      <h1 className="fruits_header-title">Fruit list</h1>
+      <FruitSearch
+        searchFruit={searchFruit}
+        handleSearchChange={handleSearchChange}
+      />
+      <FruitTable filterFruits={filterFruits} />
     </div>
   );
 };
