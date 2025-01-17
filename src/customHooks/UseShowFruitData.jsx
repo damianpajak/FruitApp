6ceb6aea3a77d23108365eac63c2bc65f50f16f4
Damian Pająk <damian.pajak01@gmail.com>
2025-01-17 14:09:39 +0100

    Create custom Hook to get all data from API

---
+++ src/customHooks/UseShowFruitData.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { getAllFruits } from "../data/getAllFruits";
+
+export const useShowFruitData = () => {
+  const [fruits, setFruits] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getFruits = async () => {
+      try {
+        const response = await getAllFruits();
+        setFruits(response);
+      } catch (error) {
+        console.log(error);
+        setError("Ops...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getFruits();
+  }, []);
+
+  return { fruits, loading, error };
+};
