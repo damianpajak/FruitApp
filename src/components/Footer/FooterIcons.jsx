323100c54e9a7f33ee786bca4bbfdacaef6ae2bf
Damian Pająk <damian.pajak01@gmail.com>
2025-01-24 22:01:46 +0100

    Add footer icons

---
+++ src/components/Footer/FooterIcons.jsx
@@ -0,0 +1,22 @@
+export const FooterIcons = () => {
+  return (
+    <div className="footer_icons">
+      <p>Author&apos;s social media contact</p>
+      <a href="https://github.com/damianpajak" target="_blank">
+        <i className="fa-brands fa-github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/damian-paj%C4%85k-225013151/"
+        target="_blank"
+      >
+        <i className="fa-brands fa-linkedin" />
+      </a>
+      <a href="https://www.instagram.com/damian_paj/?hl=pl" target="_blank">
+        <i className="fa-brands fa-instagram" />
+      </a>
+      <a href="https://www.facebook.com/damian.pajak01/" target="_blank">
+        <i className="fa-brands fa-facebook" />
+      </a>
+    </div>
+  );
+};
