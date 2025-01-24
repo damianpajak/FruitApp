b225cdecfe01ee26fc3f00bf0257e724f7f4da0e
Damian Pająk <damian.pajak01@gmail.com>
2025-01-24 21:51:13 +0100

    Add new styling for nav

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -1,12 +1,21 @@
 import { NavLink, Outlet } from "react-router";
 
 export const Navigation = () => {
+  const activePage = () => ({
+    borderBottom: "0.5rem solid #c7d36f",
+    color: "#c7d36f",
+  });
+
   return (
     <>
       <nav className="nav">
         <div className="nav-container container">
           <div className="nav-container_logo">
-            <img src="src\img\fruits_logo.png" style={{ width: 50 }} />
+            <img
+              src="src\img\fruits_logo.png"
+              alt="Fruit logo"
+              style={{ width: 50 }}
+            />
             <p>
               <span>Fruit</span>
               <span>App</span>
@@ -14,17 +23,30 @@ export const Navigation = () => {
           </div>
           <ul className="nav-container_menu">
             <li>
-              <NavLink className="nav-container_menu-link" to="/" end>
+              <NavLink
+                style={({ isActive }) => (isActive ? activePage() : {})}
+                className="nav-container_menu-link"
+                to="/"
+                end
+              >
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink className="nav-container_menu-link" to="/fruits">
+              <NavLink
+                style={({ isActive }) => (isActive ? activePage() : {})}
+                className="nav-container_menu-link"
+                to="/fruits"
+              >
                 Fruits
               </NavLink>
             </li>
             <li>
-              <NavLink className="nav-container_menu-link" to="fruit-app">
+              <NavLink
+                style={({ isActive }) => (isActive ? activePage() : {})}
+                className="nav-container_menu-link"
+                to="fruit-app"
+              >
                 Fruit App
               </NavLink>
             </li>
