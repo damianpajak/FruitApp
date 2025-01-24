b6ffc67ebf1554f901a380ede9c0cf6c5d822824
Damian Pająk <damian.pajak01@gmail.com>
2025-01-24 21:57:26 +0100

    Add new footer styling

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,11 +1,18 @@
+import { FooterIcons } from "../components/Footer/FooterIcons";
+
 export const Footer = () => {
   return (
-    <>
-      <footer className="footer">
-        <p>Final project for the JavaScript Developer course from Coders Lab</p>
-        <p>Created by Damian Pająk</p>
-        <p>Copyrights &#169; 2024</p>
-      </footer>
-    </>
+    <footer className="footer">
+      <div className="footer-container container">
+        <div>
+          <p className="footer_text">
+            Final project for the JavaScript Developer course from Coders Lab
+          </p>
+          <p className="footer_text">Created by Damian Pająk</p>
+          <p className="footer_text">Copyrights &#169; 2024</p>
+        </div>
+        <FooterIcons />
+      </div>
+    </footer>
   );
 };
