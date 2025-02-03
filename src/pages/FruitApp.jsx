bcccc58cb37801d099fa1ec81c45c11a1e988222
Damian Pająk <damian.pajak01@gmail.com>
2025-02-03 23:26:22 +0100

    Basic structure for fruit app

--- src/pages/FruitApp.jsx
+++ src/pages/FruitApp.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { ShowNewRecipe } from "../components/FruitAppPage/ShowNewRecipe";
+import { ShowRecipe } from "../components/FruitAppPage/ShowRecipe";
+import { ShowSelectedFruitsCalc } from "../components/FruitAppPage/ShowSelectedFruitsCalc";
+import { useChangeForm } from "../customHooks/UseChangeForm";
+import { useShowFruitData } from "../customHooks/UseShowFruitData";
+
 export const FruitApp = () => {
+  const { formValues, setFormValues, handleTyping } = useChangeForm();
+  const { fruits } = useShowFruitData();
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [openCreate, setOpenCreator] = useState(false);
+  const [buttonCreator, setButtonCreator] = useState(true);
+
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+    setFormValues(recipe || {});
+    setOpenCreator(false);
+    setButtonCreator(true);
+  };
+
   return (
-    <div className="container">
-      <p>Here, will be aplication to create fruit salad with data calculator</p>
+    <div className="fruitApp container">
+      <ShowRecipe onSelectRecipe={handleSelectRecipe} />
+      <ShowNewRecipe
+        formValues={formValues}
+        fruits={fruits}
+        handleTyping={handleTyping}
+        setOpenCreator={setOpenCreator}
+        openCreate={openCreate}
+        setSelectedRecipe={setSelectedRecipe}
+        buttonCreator={buttonCreator}
+        setButtonCreator={setButtonCreator}
+        setFormValues={setFormValues}
+      />
+      <ShowSelectedFruitsCalc
+        openCreate={openCreate}
+        formValues={formValues}
+        selectedRecipe={selectedRecipe}
+      />
     </div>
   );
 };
